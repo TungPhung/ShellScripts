4b98f7ed2e67ffcbfd6acf14d83c530e5e9a3654
Tung Phung <ttp2d@virginia.edu>
2019-12-19 01:56:28 -0500

    first commit

---
+++ comment.sh
@@ -0,0 +1,5 @@
+sed -i '' '8,9 s/^/# /' dayonept1.py 
+sed -i '' '12 s/^/# /' dayonept1.py 
+
+sed -i '' '8,9 s/^/# /' ../Day2/daytwopt1.py 
+sed -i '' '12 s/^/# /' ../Day2/daytwopt1.py 
---
+++ uncomment.sh
@@ -0,0 +1,2 @@
+sed -i '' '8,9 s/# //' dayonept1.py 
+sed -i '' '12 s/# //' dayonept1.py 
